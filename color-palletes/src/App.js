1dd314ff0fc397879d786624fffdec583019cbd0
VikiMaiselman <maiselmanwikki@gmail.com>
2024-01-16 11:10:14 +0200

    add Router and Routes - basic, w/o home page yet

--- color-palletes/src/App.js
+++ color-palletes/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import Palette from "./components/Palette";
 
@@ -7,10 +8,14 @@ import generateColors from "./colorHelpers";
 import "./App.css";
 
 function App() {
-  const newPalette = generateColors(seedColors[3]);
+  // const newPalette = generateColors(seedColors[3]);
   return (
     <div className="App">
-      <Palette palette={newPalette} />
+      {/* <Palette palette={newPalette} /> */}
+      <Routes>
+        {/* <Route path="/" element/> */}
+        <Route path="/palette/:paletteId" element={<Palette />} />
+      </Routes>
     </div>
   );
 }
--- color-palletes/src/components/Palette.jsx
+++ color-palletes/src/components/Palette.jsx
@@ -1,22 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import seedColors from "../seedColors";
+import generateColors from "../colorHelpers";
 
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import "../styles/Palette.css";
 
-export default function Palette({ palette }) {
-  const { colors, emoji, id, paletteName } = palette;
-
+export default function Palette() {
   const defaultShadeLevel = 500;
   const [shadeLevel, setShadeLevel] = useState(defaultShadeLevel);
   const [colorFormat, setColorFormat] = useState("hex");
+  const [palette, setPalette] = useState();
 
-  const colorBoxes = React.Children.toArray(
-    colors[shadeLevel].map((color) => {
-      console.log("color", color);
-      return <ColorBox color={color} colorFormat={colorFormat} />;
-    })
-  );
+  const { paletteId } = useParams();
+
+  useEffect(() => {
+    const seedIndex = seedColors.findIndex(
+      (palette) => palette.id === paletteId
+    );
+    const generatedPalette = generateColors(seedColors[seedIndex]);
+    setPalette(generatedPalette);
+  }, []);
+
+  const colorBoxes =
+    palette &&
+    React.Children.toArray(
+      palette.colors[shadeLevel].map((color) => {
+        // console.log("color", color);
+        return <ColorBox color={color} colorFormat={colorFormat} />;
+      })
+    );
 
   const handleLevelChange = (newShadeLevel) => {
     setShadeLevel(newShadeLevel);
@@ -27,22 +42,24 @@ export default function Palette({ palette }) {
   };
 
   return (
-    <div className="Palette">
-      <Navbar
-        defaultShadeLevel={defaultShadeLevel}
-        shadeLevel={shadeLevel}
-        handleLevelChange={handleLevelChange}
-        handleColorFormatChange={handleColorFormatChange}
-      />
+    palette && (
+      <div className="Palette">
+        <Navbar
+          defaultShadeLevel={defaultShadeLevel}
+          shadeLevel={shadeLevel}
+          handleLevelChange={handleLevelChange}
+          handleColorFormatChange={handleColorFormatChange}
+        />
 
-      {/* a list of colorBoxes */}
-      <div className="Palette-colors">{colorBoxes}</div>
+        {/* a list of colorBoxes */}
+        <div className="Palette-colors">{colorBoxes}</div>
 
-      <footer className="Palette-footer">
-        <span className="footer-emoji">{emoji}</span>
-        {paletteName}
-        <span className="footer-emoji">{emoji}</span>
-      </footer>
-    </div>
+        <footer className="Palette-footer">
+          <span className="footer-emoji">{palette.emoji}</span>
+          {palette.paletteName}
+          <span className="footer-emoji">{palette.emoji}</span>
+        </footer>
+      </div>
+    )
   );
 }
